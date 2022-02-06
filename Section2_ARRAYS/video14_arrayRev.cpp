3ce40d46e290334bd4aa994d919cfcf249701d6c
ngarg2k2 <garg2002nipun@gmail.com>
2022-02-06 23:29:18 +0530

    revArray

---
+++ Section2_ARRAYS/video14_arrayRev.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+void swap(int* a,int* b){
+    int temp=*a;
+    *a=*b;
+    *b=temp;
+}
+void reverseArray(int arr[],int n){
+    int i=0;
+    int j=n-1;
+    while(i<=j){
+        swap(arr[i],arr[j]);
+        i++;
+        j--;
+    }
+}
+void display(int arr[],int n){
+    for (int i = 0; i < n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+int main(){
+    int arr[]={1,5,6,9,4,2,7};
+    int n=sizeof(arr)/sizeof(int);
+    display(arr,n);
+    reverseArray(arr,n);
+    display(arr,n);
+    return 0;
+}
