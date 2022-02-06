b66099503404951dd1c61786b679b3ee14bbc5bd
ngarg2k2 <garg2002nipun@gmail.com>
2022-02-06 23:41:47 +0530

    print pair

---
+++ Section2_ARRAYS/video14_printPair.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[]={1,2,3,4,5,6};
+    int n=sizeof(arr)/sizeof(int);
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i+1; j < n; j++)
+        {
+            cout<<"("<<arr[i]<<","<<arr[j]<<")"<<" ";       
+        }
+        cout<<endl;
+    }
+    
+}
