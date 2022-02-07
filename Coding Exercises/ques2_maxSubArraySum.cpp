08f8cb2578de3783078ac3de3ea786ddda416f14
ngarg2k2 <garg2002nipun@gmail.com>
2022-02-07 14:15:44 +0530

    coding ex ques 2 max sum of sub array

---
+++ Coding Exercises/ques2_maxSubArraySum.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int maxSumSubarray(vector<int> A) {
+    vector<int>preArr(A.size());
+    preArr[0]=A[0];
+    for (int i = 1; i < A.size(); i++)
+    {
+        preArr[i]=preArr[i-1]+A[i];
+    }
+    int maxSum=0;
+    for (int i = 0; i < A.size(); i++)
+    {
+        for (int j = 0; j < A.size(); j++)
+        {
+            int sum=0;
+            sum=i>0?preArr[j]-preArr[i-1]:preArr[j];
+            maxSum=max(maxSum,sum);
+        }
+    }
+    return maxSum;
+}
+int main(){
+    vector<int>arr(6);
+
+    for (int i = 0; i < arr.size(); i++)
+    {
+        cin>>arr[i];
+    }
+
+    cout<<maxSumSubarray(arr)<<endl;
+    
+}
