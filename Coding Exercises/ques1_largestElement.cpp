066761e6394cab79a791d71f762faf4d388205e7
ngarg2k2 <garg2002nipun@gmail.com>
2022-02-07 14:10:03 +0530

    Coding ex ques1

---
+++ Coding Exercises/ques1_largestElement.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int largestElement(vector<int>arr){
+    int max=arr[0];
+    for (int i = 0; i < arr.size(); i++)
+    {
+        if(arr[i]>max){
+            max=arr[i];
+        }
+    }
+    return max;
+}
+int main(){
+    vector<int>arr(5);
+    // cout<<arr.size();
+    for (int i = 0; i < arr.size(); i++)
+    {
+        cin>>arr[i];
+    }
+    cout<<largestElement(arr);
+    
+}
