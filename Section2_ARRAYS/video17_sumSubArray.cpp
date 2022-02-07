79d43fd6aa1613db84d31c76a28dca247e80d777
ngarg2k2 <garg2002nipun@gmail.com>
2022-02-07 10:44:51 +0530

    brute force

--- Section2_ARRAYS/video17_sumSubArray.cpp
+++ Section2_ARRAYS/video17_sumSubArray.cpp
@@ -1,20 +1,25 @@
+//Brute force
 #include<iostream>
 using namespace std;
 int main(){
-    int arr[]={1,2,3,4,5,6};
+    int arr[]={1,2,3,4,5};
     int n=sizeof(arr)/sizeof(int);
+    int maxSum=0;
 
     for (int i = 0; i < n; i++)
     {
         for (int j = i; j < n; j++)
         {
+            int sum=0;
             for (int k = i; k <= j; k++)
             {
-                cout<<arr[k]<<",";
+                sum=sum+arr[k];
+                cout<<sum<<" ,";
             }
             cout<<endl;
+            maxSum=max(maxSum,sum);
         }
         
     }
-    
+    cout<<"Max Sum: "<<maxSum<<endl;
 }
