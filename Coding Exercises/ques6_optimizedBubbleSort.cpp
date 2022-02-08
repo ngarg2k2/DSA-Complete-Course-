4890074f87b6065865b95493b65e56e6ba8d2145
ngarg2k2 <garg2002nipun@gmail.com>
2022-02-08 18:20:08 +0530

    ques 6 bubble sort

---
+++ Coding Exercises/ques6_optimizedBubbleSort.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+vector<int> bubbleSort(vector<int> arr)
+{
+    for (int i = 0; i < arr.size() - 1; i++)
+    {
+        bool flag = false;
+        for (int j = 0; j < arr.size() - i; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                flag = true;
+                swap(arr[j], arr[j + 1]);
+            }
+            for (int i = 0; i < arr.size(); i++)
+            {
+                // cout<<arr[i]<<" ";
+            }
+            cout << endl;
+        }
+        if (flag == false)
+        {
+            return arr;
+        }
+    }
+    return arr;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < arr.size(); i++)
+    {
+        cin >> arr[i];
+    }
+
+    arr = bubbleSort(arr);
+
+    for (int i = 0; i < arr.size(); i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
