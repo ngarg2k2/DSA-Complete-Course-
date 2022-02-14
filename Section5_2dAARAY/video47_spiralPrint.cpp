4b70173f52ba64e4d49dba67e73ddb34ad66152e
ngarg2k2 <garg2002nipun@gmail.com>
2022-02-14 21:55:52 +0530

    updated

---
+++ Section5_2dAARAY/video47_spiralPrint.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+void printSpiral(int a[100][100],int n,int m){
+
+}
+
+int main(){
+    int a[100][100];
+    int n,m;
+    cin>>n>>m;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cin>>a[i][j];
+        }
+
+    }
+    
+    return 0;
+}
